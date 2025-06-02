6e34e0a5e23a528c61bbc1681b15b7ab5a0d0c3c
saroj-2728 <080bct075.saroj@pcampus.edu.np>
2025-06-02 23:35:05 +0545

    refactor hypr config to multiple files, refactor waybar and rofi, add wifi menu display on rofi

---
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+WALLPAPER_DIR="$HOME/.config/backgrounds/"
+
+# Get a random wallpaper
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+# Apply the same wallpaper to all monitors
+hyprctl hyprpaper reload ,"$WALLPAPER"
